939cf3b5f53a706579adda522f6212476f969a3d
Saufianem <cherief.saufianee@gmail.com>
2021-03-19 11:44:06 +0100

    modif

--- index.php
+++ index.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/style.css"/>
     <title>Projet JS</title>
   </head>
   <body>
-    <h1>Combattants Internationaux</h1>
-    <form>
-      <label for="name">Veuillez choisir un combattant:</label>
+  <div>
+  <h1 class="title text-white ">Combattants Internationaux</h1>
+  </div>
+  <div>
+    <form class="form_so">
+      <label class="text-white" for="name">Veuillez choisir un combattant:</label>
       <input list="fighters" name="name" id="name" />
       <datalist id="fighters"> </datalist>
       <input type="submit" id="btn" value="Valider" />
     </form>
-    <div id="combattants"></div>
+    </div>
+    <div class="text-white" id="combattants"></div>
     <script src="assets/js/app.js"></script>
   </body>
 </html>
