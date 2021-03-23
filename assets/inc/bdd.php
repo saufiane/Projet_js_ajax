25bd05432d97cdb33a8bdcf5757d1941c76a81ca
Saufianem <cherief.saufianee@gmail.com>
2021-03-23 16:45:48 +0100

    bdd en local & o2switch

--- assets/inc/bdd.php
+++ assets/inc/bdd.php
@@ -1,7 +1,12 @@
 <?php
 
 try{
+    // bdd en local avec php my admin
     $db = new PDO('mysql:host=127.0.0.1;dbname=projet_js', 'root','');
+
+    // bdd sur O2switch
+    
+    // $db = new PDO('mysql:host=127.0.0.1;dbname=wbmq4652_projet_js', 'wbmq4652_saufiane','sosososo=789');
     $db->exec('SET NAMES "UTF8"');
     }catch (PDOException $e){
     echo 'Erreur : '. $e->getMessage();
